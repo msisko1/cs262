b03d2d247a5c8873f42adad621249304705cb64a
msisko1 <00+msisko1@users.noreply.github.com>
2020-10-02 18:33:29 -0400

    new file:   lab05/README.md 	new file:   lab05/gamezone/.expo-shared/assets.json 	new file:   lab05/gamezone/.gitignore 	new file:   lab05/gamezone/App.js 	new file:   lab05/gamezone/app.json 	new file:   lab05/gamezone/assets/fonts/Nunito-Bold.ttf 	new file:   lab05/gamezone/assets/fonts/Nunito-Regular.ttf 	new file:   lab05/gamezone/assets/icon.png 	new file:   lab05/gamezone/assets/splash.png 	new file:   lab05/gamezone/babel.config.js 	new file:   lab05/gamezone/package-lock.json 	new file:   lab05/gamezone/package.json 	new file:   lab05/gamezone/routes/aboutStack.js 	new file:   lab05/gamezone/routes/drawer.js 	new file:   lab05/gamezone/routes/homeStack.js 	new file:   lab05/gamezone/screens/about.js 	new file:   lab05/gamezone/screens/home.js 	new file:   lab05/gamezone/screens/reviewDetails.js 	new file:   lab05/gamezone/shared/card.js 	new file:   lab05/gamezone/shared/header.js 	new file:   lab05/gamezone/styles/global.js 	new file:   lab05/gamezone/yarn.lock

---
+++ lab05/gamezone/shared/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Header({ navigation }) {
+
+    const openMenu = () => {
+        navigation.openDrawer();
+    }
+
+    return (
+        <View>
+            <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#333',
+        letterSpacing: 1,
+    },
+    icon: {
+        position: 'absolute',
+        margin: 15,
+    },
+});
