f1df9363b29959af36b845d4e0f73ed2c2e9aea2
msisko1 <00+msisko1@users.noreply.github.com>
2020-10-18 14:54:13 -0400

    modified:   lab07/monopoly.sql

--- lab07/monopoly.sql
+++ lab07/monopoly.sql
@@ -1,19 +1,31 @@
 ﻿--
 -- This SQL script builds a monopoly database, deleting any pre-existing version.
 --
+-- @modified by: Michael Sisko
+-- @date modified: 10/18/2020
+--
 -- @author kvlinden
 -- @version Summer, 2015
---
+-- 
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
-DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
+DROP TABLE IF EXISTS GameProp;
 DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS PlayerGame;
 
 -- Create the schema.
 CREATE TABLE Game (
 	ID integer PRIMARY KEY, 
 	time timestamp
+	);
+
+CREATE TABLE GameProp (	
+	gameID integer REFERENCES Game(ID),
+	playerID integer REFERENCES Player(ID),
+	property varchar(50),
+	development varchar(50),
+	PRIMARY KEY (gameID, property)
 	);
 
 CREATE TABLE Player (
@@ -23,13 +35,17 @@ CREATE TABLE Player (
 	);
 
 CREATE TABLE PlayerGame (
-	gameID integer REFERENCES Game(ID), 
-	playerID integer REFERENCES Player(ID),
-	score integer
+    gameID integer REFERENCES Game(ID),
+    playerID integer REFERENCES Player(ID),
+    score integer,
+    cash integer,
+	position varchar(50) NOT NULL,
+    PRIMARY KEY (gameID, playerID)
 	);
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
+GRANT SELECT ON GameProp TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
 
@@ -42,11 +58,23 @@ INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
 INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
 INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
 
-INSERT INTO PlayerGame VALUES (1, 1, 0.00);
-INSERT INTO PlayerGame VALUES (1, 2, 0.00);
-INSERT INTO PlayerGame VALUES (1, 3, 2350.00);
-INSERT INTO PlayerGame VALUES (2, 1, 1000.00);
-INSERT INTO PlayerGame VALUES (2, 2, 0.00);
-INSERT INTO PlayerGame VALUES (2, 3, 500.00);
-INSERT INTO PlayerGame VALUES (3, 2, 0.00);
-INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+-- Individual player values
+INSERT INTO PlayerGame VALUES (1, 1, 1, 0.00, 'start');
+INSERT INTO PlayerGame VALUES (1, 2, 1, 0.00, 'start');
+INSERT INTO PlayerGame VALUES (1, 3, 1, 2350.00, 'start');
+INSERT INTO PlayerGame VALUES (2, 1, 1, 1000.00, 'start');
+INSERT INTO PlayerGame VALUES (2, 2, 1, 0.00, 'start');
+INSERT INTO PlayerGame VALUES (2, 3, 1, 500.00, 'start');
+INSERT INTO PlayerGame VALUES (3, 2, 1, 0.00, 'start');
+INSERT INTO PlayerGame VALUES (3, 3, 1, 5500.00, 'start');
+
+-- Game property values
+INSERT INTO GameProp VALUES (1, 1, 'St. James Place', 'hotel');
+INSERT INTO GameProp VALUES (1, 2, 'Water Works', 'none');
+INSERT INTO GameProp VALUES (1, 3, 'Ventnor Ave.', 'house');
+INSERT INTO GameProp VALUES (2, 1, 'Oriental Ave.', 'hotel');
+INSERT INTO GameProp VALUES (2, 2, 'Park Place', 'house');
+INSERT INTO GameProp VALUES (2, 3, 'Oriental Ave.', 'none');
+INSERT INTO GameProp VALUES (3, 1, 'Board Walk', 'house');
+INSERT INTO GameProp VALUES (3, 2, 'St. James Place', 'none');
+INSERT INTO GameProp VALUES (3, 3, 'Board Walk', 'none');
